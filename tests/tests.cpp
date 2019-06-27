46d300cdaad71dce13692f048a3a87f251ff79d3
Jonas Stein <news@jonasstein.de>
2019-06-27 23:53:42 +0200

    add tests

---
+++ tests/tests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+//#include <cmath> 
+//#include <cstdint>
+//#include <string>     // std::string, std::stoull
+
+//const std::string testFileName = "samples/180sec_2chan_5kHz_pm_4kHz_FM_4Hz_Trigger1Hz.mdat";
+
+TEST(CategoryTest, SpecificTest)
+{
+    ASSERT_EQ(0, 0);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
