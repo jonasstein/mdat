5498beaedd8244e6a235a0ae6e70bbfabcee4ee2
Jonas Stein <news@jonasstein.de>
2019-11-02 14:09:23 +0100

    add build folder

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-rm -Rf CMakeFiles/ CMakeCache.txt && cmake -DCMAKE_CXX_COMPILER=clang++ .  && make
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname ${SCRIPT}`
+
+rm -Rf CMakeFiles/ CMakeCache.txt && cmake -B ${SCRIPTPATH}/build -DCMAKE_CXX_COMPILER=clang++ .  && make
