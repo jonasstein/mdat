6ca825833f08089cdf3917d8f4bc4a01b91c4839
Jonas Stein <news@jonasstein.de>
2019-11-03 21:14:44 +0100

    clang-format

--- src/Mdatevent.cpp
+++ src/Mdatevent.cpp
@@ -1,195 +1,226 @@
 #include "Mdatevent.h"
 
-#include <iostream>     // std::cout
-#include <cstdint>      // int8_t
 #include "Bitslicer.h"
-
+#include <cstdint>  // int8_t
+#include <iostream> // std::cout
 
 namespace mevent {
 
-Mdatevent::Mdatevent() :
-		EventID(IDClass::neutron), EventTimestamp(0b0),
-		EventModID(0), EventSlotID(0), EventAmplitude(0), EventPosition(0),
-		EventTrigID(TrigIDClass::CmpReg), EventDataID(DataIDClass::ADC1), EventData(0b0) {
-}
+Mdatevent::Mdatevent()
+    : EventID(IDClass::neutron), EventTimestamp(0b0), EventModID(0),
+      EventSlotID(0), EventAmplitude(0), EventPosition(0),
+      EventTrigID(TrigIDClass::CmpReg), EventDataID(DataIDClass::ADC1),
+      EventData(0b0) {}
 
 Mdatevent::Mdatevent(IDClass myid, TimestampClass mytimestamp,
-		ModIDClass EventModID, SlotIDClass EventSlotID, AmplitudeClass EventAmplitude, PositionClass EventPosition,
-		TrigIDClass mytrigid, DataIDClass mydataid, DataClass mydata) :
-		EventID(myid), EventTimestamp(mytimestamp),
-		EventModID(0), EventSlotID(0), EventAmplitude(0), EventPosition(0),
-		EventTrigID(mytrigid), EventDataID(mydataid), EventData(mydata) {
-//	std::cout << "Constructor was called\n";
+                     ModIDClass EventModID, SlotIDClass EventSlotID,
+                     AmplitudeClass EventAmplitude, PositionClass EventPosition,
+                     TrigIDClass mytrigid, DataIDClass mydataid,
+                     DataClass mydata)
+    : EventID(myid), EventTimestamp(mytimestamp), EventModID(0), EventSlotID(0),
+      EventAmplitude(0), EventPosition(0), EventTrigID(mytrigid),
+      EventDataID(mydataid), EventData(mydata) {
+  //	std::cout << "Constructor was called\n";
 }
 
+Mdatevent::~Mdatevent() {}
 
+IDClass Mdatevent::getEventID(uint64_t rawinteger) {
+  mevent::IDClass myreturn;
 
-Mdatevent::~Mdatevent() {
+  switch (rawinteger) {
+  case 0: {
+    myreturn = mevent::IDClass::neutron;
+    break;
+  }
+  default:
+    myreturn = mevent::IDClass::trigger;
+  }
+  return myreturn;
 }
 
-IDClass Mdatevent::getEventID(uint64_t rawinteger){
-	mevent::IDClass myreturn;
+TrigIDClass Mdatevent::getTrigID(uint64_t rawinteger) {
+  mevent::TrigIDClass myreturn;
 
-	switch(rawinteger) {
-	    case 0: {  myreturn = mevent::IDClass::neutron;
-	               break;
-	            }
-	    default: myreturn = mevent::IDClass::trigger;
-	}
-	return myreturn;
+  switch (rawinteger) {
+  case 1: {
+    myreturn = mevent::TrigIDClass::Timer1;
+    break;
+  }
+  case 2: {
+    myreturn = mevent::TrigIDClass::Timer2;
+    break;
+  }
+  case 3: {
+    myreturn = mevent::TrigIDClass::Timer3;
+    break;
+  }
+  case 4: {
+    myreturn = mevent::TrigIDClass::Timer4;
+    break;
+  }
+  case 5: {
+    myreturn = mevent::TrigIDClass::RearTTL1;
+    break;
+  }
+  case 6: {
+    myreturn = mevent::TrigIDClass::RearTTL2;
+    break;
+  }
+  default: {
+    myreturn = mevent::TrigIDClass::CmpReg;
+    break;
+  }
+  }
+
+  return myreturn;
 }
 
+DataIDClass Mdatevent::getDataID(uint64_t rawinteger) {
+  mevent::DataIDClass myreturn;
 
-TrigIDClass Mdatevent::getTrigID(uint64_t rawinteger){
-	mevent::TrigIDClass myreturn;
+  switch (rawinteger) {
+  case 0: {
+    myreturn = mevent::DataIDClass::Monitor1;
+    break;
+  }
+  case 1: {
+    myreturn = mevent::DataIDClass::Monitor2;
+    break;
+  }
+  case 2: {
+    myreturn = mevent::DataIDClass::Monitor3;
+    break;
+  }
+  case 3: {
+    myreturn = mevent::DataIDClass::Monitor4;
+    break;
+  }
+  case 4: {
+    myreturn = mevent::DataIDClass::RearTTL1;
+    break;
+  }
+  case 5: {
+    myreturn = mevent::DataIDClass::RearTTL2;
+    break;
+  }
+  case 6: {
+    myreturn = mevent::DataIDClass::ADC1;
+    break;
+  }
+  default: {
+    myreturn = mevent::DataIDClass::ADC2;
+    break;
+  }
+  }
 
-	switch(rawinteger) {
-	case 1: {  myreturn = mevent::TrigIDClass::Timer1;
-	break;}
-	case 2: {  myreturn = mevent::TrigIDClass::Timer2;
-	break;}
-	case 3: {  myreturn = mevent::TrigIDClass::Timer3;
-	break;}
-	case 4: {  myreturn = mevent::TrigIDClass::Timer4;
-	break;}
-	case 5: {  myreturn = mevent::TrigIDClass::RearTTL1;
-	break;}
-	case 6: {  myreturn = mevent::TrigIDClass::RearTTL2;
-	break;}
-	default: {  myreturn = mevent::TrigIDClass::CmpReg;
-	break;}
-	}
-
-	return myreturn;
+  return myreturn;
 }
-
-DataIDClass Mdatevent::getDataID(uint64_t rawinteger){
-	mevent::DataIDClass myreturn;
-
-	switch(rawinteger) {
-	case 0: {  myreturn = mevent::DataIDClass::Monitor1;
-	break;}
-	case 1: {  myreturn = mevent::DataIDClass::Monitor2;
-	break;}
-	case 2: {  myreturn = mevent::DataIDClass::Monitor3;
-	break;}
-	case 3: {  myreturn = mevent::DataIDClass::Monitor4;
-	break;}
-	case 4: {  myreturn = mevent::DataIDClass::RearTTL1;
-	break;}
-	case 5: {  myreturn = mevent::DataIDClass::RearTTL2;
-	break;}
-	case 6: {  myreturn = mevent::DataIDClass::ADC1;
-		break;}
-	default: {  myreturn = mevent::DataIDClass::ADC2;
-	break;}
-	}
-
-	return myreturn;
-}
-
-
 
 Mdatevent Mdatevent::neutronevent(TimestampClass mytimestamp,
-		ModIDClass EventModID, SlotIDClass EventSlotID, AmplitudeClass EventAmplitude, PositionClass EventPosition) {
+                                  ModIDClass EventModID,
+                                  SlotIDClass EventSlotID,
+                                  AmplitudeClass EventAmplitude,
+                                  PositionClass EventPosition) {
 
-	mevent::Mdatevent mynewevent{mevent::IDClass::neutron, mytimestamp,
-		EventModID, EventSlotID, EventAmplitude, EventPosition,
-		mevent::TrigIDClass::Timer1,mevent::DataIDClass::Monitor1, 0};
-	return mynewevent;
+  mevent::Mdatevent mynewevent{mevent::IDClass::neutron,
+                               mytimestamp,
+                               EventModID,
+                               EventSlotID,
+                               EventAmplitude,
+                               EventPosition,
+                               mevent::TrigIDClass::Timer1,
+                               mevent::DataIDClass::Monitor1,
+                               0};
+  return mynewevent;
 }
-
 
 Mdatevent Mdatevent::triggerevent(TimestampClass mytimestamp,
-		TrigIDClass mytrigid, DataIDClass mydataid, DataClass mydata) {
+                                  TrigIDClass mytrigid, DataIDClass mydataid,
+                                  DataClass mydata) {
 
-	mevent::Mdatevent mynewevent{mevent::IDClass::trigger, mytimestamp,
-			0, 0, 0, 0,
-			mytrigid, mydataid, mydata};
-		return mynewevent;
+  mevent::Mdatevent mynewevent{mevent::IDClass::trigger,
+                               mytimestamp,
+                               0,
+                               0,
+                               0,
+                               0,
+                               mytrigid,
+                               mydataid,
+                               mydata};
+  return mynewevent;
 }
 
+Mdatevent Mdatevent::importrawevent(char rawinput[6]) {
 
-Mdatevent Mdatevent::importrawevent(char rawinput [6]) {
+  uint8_t LoWordA = rawinput[0];
+  uint8_t LoWordB = rawinput[1];
+  uint8_t MidWordA = rawinput[2];
+  uint8_t MidWordB = rawinput[3];
+  uint8_t HiWordA = rawinput[4];
+  uint8_t HiWordB = rawinput[5];
 
+  uint64_t fullevent = HiWordA << 40 + HiWordB << 32 + MidWordA << 24 + MidWordB
+                               << 16 + LoWordA << 8 + LoWordB;
 
-	uint8_t LoWordA  = rawinput[0];
-	uint8_t LoWordB  = rawinput[1];
-	uint8_t MidWordA = rawinput[2];
-	uint8_t MidWordB = rawinput[3];
-	uint8_t HiWordA  = rawinput[4];
-	uint8_t HiWordB  = rawinput[5];
+  u_int64_t myID =
+      (fullevent & 0b100000000000000000000000000000000000000000000000) >> 47;
+  u_int64_t mytrigid =
+      (fullevent & 0b011100000000000000000000000000000000000000000000) >> 44;
+  u_int64_t mydataid =
+      (fullevent & 0b000011110000000000000000000000000000000000000000) >> 40;
+  u_int64_t mydata =
+      (fullevent & 0b000000001111111111111111111110000000000000000000) >> 19;
+  u_int64_t mytimestamp =
+      (fullevent & 0b000000000000000000000000000001111111111111111111);
 
-	uint64_t fullevent = HiWordA  << 40 +
-			             HiWordB  << 32 +
-						 MidWordA << 24 +
-						 MidWordB << 16 +
-						 LoWordA  <<  8 +
-						 LoWordB;
-
-     u_int64_t myID        = (fullevent & 0b100000000000000000000000000000000000000000000000) >> 47;
-	 u_int64_t mytrigid    = (fullevent & 0b011100000000000000000000000000000000000000000000) >> 44;
-	 u_int64_t mydataid    = (fullevent & 0b000011110000000000000000000000000000000000000000) >> 40;
-	 u_int64_t mydata      = (fullevent & 0b000000001111111111111111111110000000000000000000) >> 19;
-	 u_int64_t mytimestamp = (fullevent & 0b000000000000000000000000000001111111111111111111);
-
-
-
-	mevent::Mdatevent mynewevent{};
-	mynewevent.triggerevent(mytimestamp, mevent::TrigIDClass::Timer1 , mevent::DataIDClass::ADC1 , mydata);
-	return mynewevent;
+  mevent::Mdatevent mynewevent{};
+  mynewevent.triggerevent(mytimestamp, mevent::TrigIDClass::Timer1,
+                          mevent::DataIDClass::ADC1, mydata);
+  return mynewevent;
 }
 
+void Mdatevent::printeventverbose(void) {
 
-
-
-void Mdatevent::printeventverbose(void){
-
-
-	if (this->EventID == IDClass::neutron){
-	printf("EventID %d (neutron event), ", this->EventID);
+  if (this->EventID == IDClass::neutron) {
+    printf("EventID %d (neutron event), ", this->EventID);
     printf("EventTimestamp %d, ", this->EventTimestamp);
 
-	printf("EventModID %d, ", this->EventModID);
-	printf("EventSlotID %d, ", this->EventSlotID);
-	printf("EventAmplitude %d, ", this->EventAmplitude);
-	printf("EventPosition %d\n", this->EventPosition);
-	}
+    printf("EventModID %d, ", this->EventModID);
+    printf("EventSlotID %d, ", this->EventSlotID);
+    printf("EventAmplitude %d, ", this->EventAmplitude);
+    printf("EventPosition %d\n", this->EventPosition);
+  }
 
-	if (this->EventID == IDClass::trigger){
-	printf("EventID %d (trigger event), ", this->EventID);
-	printf("EventTimestamp %d, ", this->EventTimestamp);
+  if (this->EventID == IDClass::trigger) {
+    printf("EventID %d (trigger event), ", this->EventID);
+    printf("EventTimestamp %d, ", this->EventTimestamp);
 
-	printf("EventTrigID %d, ", this->EventTrigID);
-	printf("EventDataID %d, ", this->EventDataID);
-	printf("EventData %d\n", this->EventData);
-	}
+    printf("EventTrigID %d, ", this->EventTrigID);
+    printf("EventDataID %d, ", this->EventDataID);
+    printf("EventData %d\n", this->EventData);
+  }
 }
 
+void Mdatevent::printevent(void) {
+  if (this->EventID == IDClass::neutron) {
+    printf("%d, ", this->EventTimestamp);
+    printf("8, "); // 0..7 are trigger events 8 are neutron events
+    printf("%d, ", this->EventModID);
+    printf("%d, ", this->EventSlotID);
+    printf("%d, ", this->EventAmplitude);
+    printf("%d\n", this->EventPosition);
+  }
 
-
-void Mdatevent::printevent(void){
-	if (this->EventID == IDClass::neutron){
-		printf("%d, ", this->EventTimestamp);
-		printf("8, ");  // 0..7 are trigger events 8 are neutron events
-		printf("%d, ", this->EventModID);
-		printf("%d, ", this->EventSlotID);
-		printf("%d, ", this->EventAmplitude);
-		printf("%d\n", this->EventPosition);
-		}
-
-		if (this->EventID == IDClass::trigger){
-		printf("%d, ", this->EventTimestamp);
-		printf("%d, ", this->EventTrigID);
-		printf("%d, ", this->EventDataID);
-		printf("%d\n", this->EventData);
-		}
+  if (this->EventID == IDClass::trigger) {
+    printf("%d, ", this->EventTimestamp);
+    printf("%d, ", this->EventTrigID);
+    printf("%d, ", this->EventDataID);
+    printf("%d\n", this->EventData);
+  }
 }
-
-
 
 } /* namespace mevent */
-
 
 // new neutronfromraw
 // new neutronfromdata
@@ -197,7 +228,6 @@ void Mdatevent::printevent(void){
 // new triggerfromdata
 // isneutron
 // istrigger
-
 
 /*
  * Neutron events
