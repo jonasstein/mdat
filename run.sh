ec59d74d5f4180cf6c4d49b78d181c58aa966a8f
Jonas Stein <news@jonasstein.de>
2019-11-17 21:11:58 +0100

    test only if compiler+linker exit without error

--- run.sh
+++ run.sh
@@ -13,8 +13,7 @@ rm -rf CMakeFiles/ CMakeCache.txt
 rm -rf ${BUILDPATH}/*
 
 cmake -B ${BUILDPATH} -DCMAKE_CXX_COMPILER=clang++ .  && make && \
-
-(cd ${SCRIPTPATH}/tests && ./Bitslicer_test) 
-(cd ${SCRIPTPATH}/tests && ./Lmfile_test)
+(cd ${SCRIPTPATH}/tests && ./Bitslicer_test) && \
+(cd ${SCRIPTPATH}/tests && ./Lmfile_test) && \
 (cd ${SCRIPTPATH}/tests && ./Mdatevent_test)
 
