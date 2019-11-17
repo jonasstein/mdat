991625c34cd02627557b95c8ce0c26555527e0d9
Jonas Stein <news@jonasstein.de>
2019-11-17 17:50:51 +0100

    run tests in subshell in tests/ folder

--- run.sh
+++ run.sh
@@ -13,7 +13,8 @@ rm -rf CMakeFiles/ CMakeCache.txt
 rm -rf ${BUILDPATH}/*
 
 cmake -B ${BUILDPATH} -DCMAKE_CXX_COMPILER=clang++ .  && make && \
-${SCRIPTPATH}/tests/Bitslicer_test && \
-${SCRIPTPATH}/tests/Lmfile_test && \
-${SCRIPTPATH}/tests/Mdatevent_test  
+
+(cd ${SCRIPTPATH}/tests && ./Bitslicer_test) 
+(cd ${SCRIPTPATH}/tests && ./Lmfile_test)
+(cd ${SCRIPTPATH}/tests && ./Mdatevent_test)
 
--- tests/Lmfile_test.cpp
+++ tests/Lmfile_test.cpp
@@ -5,14 +5,20 @@
 
 #include "../src/Lmfile.h"
 
-const std::string testFileName = "../tests/39events.mdat";
+const std::string testFileA = "39events.mdat";
 
 TEST(lmfile, set_read_verbosity)
 {
 //	uint64_t sorted = 0b100000000000000000000000000000000000000000000000;
 //	mevent::TimestampClass myBuffertime_ns {0x0 * 100};
 //	mevent::set
+	mfile::Lmfile *limo;
+    limo = new mfile::Lmfile(testFileA, 0);
+    ASSERT_EQ(limo->getfilesize(), 308);
+    delete limo;
+
 }
+
 
 
 int main(int argc, char **argv)
