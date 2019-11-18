0964b0d2523d35db9b2cf34ad1bb9d90a198feaf
Jonas Stein <news@jonasstein.de>
2019-11-18 03:44:36 +0100

    add missing file

---
+++ src/Globaltypes.h
@@ -0,0 +1,8 @@
+#ifndef SRC_GLOBALTYPES_H_
+#define SRC_GLOBALTYPES_H_
+
+#include <cstdint> // int8_t
+using TimestampClass = uint64_t; // 3x16 = 48 bit offset from the header
+
+
+#endif /* SRC_GLOBALTYPES_H_ */
