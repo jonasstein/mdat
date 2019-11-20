b807c3cdc6102efc44480e7c2f39ff26421074a9
Jonas Stein <news@jonasstein.de>
2019-11-20 22:20:04 +0100

    remove delete and new

--- tests/Lmfile_test.cpp
+++ tests/Lmfile_test.cpp
@@ -10,58 +10,47 @@ const std::string testFileB = "5linesheader.mdat";
 
 TEST(lmfile, getfilesize)
 {
-	mfile::Lmfile *limo;
-    limo = new mfile::Lmfile(testFileA, 0);
-    ASSERT_EQ(limo->getfilesize(), 308);
-    delete limo;
+	mfile::Lmfile limo(testFileA, 0);
+    ASSERT_EQ(limo.getfilesize(), 308);
 }
 
 TEST(lmfile, set_read_verbosity)
 {
-	mfile::Lmfile *limo;
-    limo = new mfile::Lmfile(testFileA, 0);
-    ASSERT_EQ(limo->getverbosity(), 0);
-    delete limo;
+	mfile::Lmfile lmA(testFileA, 0);
+    ASSERT_EQ(lmA.getverbosity(), 0);
 
-    limo = new mfile::Lmfile(testFileA, 1);
-    ASSERT_EQ(limo->getverbosity(), 1);
+    mfile::Lmfile lmB (testFileA, 1);
+    ASSERT_EQ(lmB.getverbosity(), 1);
 
-    limo->setverbosity(3);
-    ASSERT_EQ(limo->getverbosity(), 3);
-    delete limo;
+    lmB.setverbosity(3);
+    ASSERT_EQ(lmB.getverbosity(), 3);
 }
 
 
 TEST(lmfile, jumpbehindfileheader_2Lines)
 {
-	mfile::Lmfile *limo;
-    limo = new mfile::Lmfile(testFileA, 0);
-    limo->jumpbehindfileheader();
-    ASSERT_EQ(limo->getposition(), 50);
-    delete limo;
+	mfile::Lmfile limo(testFileA, 0);
+    limo.jumpbehindfileheader();
+    ASSERT_EQ(limo.getposition(), 50);
 }
 
 TEST(lmfile, jumpbehindfileheader_5Lines)
 {
-	mfile::Lmfile *limo;
-    limo = new mfile::Lmfile(testFileB, 0);
-    limo->jumpbehindfileheader();
-    ASSERT_EQ(limo->getposition(), 129);
-    delete limo;
+	mfile::Lmfile limo(testFileB, 0);
+    limo.jumpbehindfileheader();
+    ASSERT_EQ(limo.getposition(), 129);
 }
 
 
 TEST(lmfile, getbytestillEOF)
 {
-	mfile::Lmfile *limo;
-    limo = new mfile::Lmfile(testFileA, 0);
+	mfile::Lmfile limo(testFileA, 0);
 
-    mfile::filesize_t fs = limo->getfilesize();
+    mfile::filesize_t fs = limo.getfilesize();
 
-    ASSERT_EQ(limo->getbytestillEOF(), fs);
-    limo->jumpbehindfileheader();
-    ASSERT_EQ(limo->getbytestillEOF(), fs-50);
-    delete limo;
+    ASSERT_EQ(limo.getbytestillEOF(), fs);
+    limo.jumpbehindfileheader();
+    ASSERT_EQ(limo.getbytestillEOF(), fs-50);
 }
 
 
@@ -70,4 +59,3 @@ int main(int argc, char **argv)
     ::testing::InitGoogleTest(&argc, argv);
     return(RUN_ALL_TESTS());
 }
-
